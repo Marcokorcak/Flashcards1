b72e19f35c490a5d8c461a4d63157592d26d8b35
Marco Korcak <marcokorcak02@gmail.com>
2021-02-23 10:59:01 -0800

    Initial Commit

---
+++ Flashcars v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcars v2
+//
+//  Created by Marco Korcak on 2/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
